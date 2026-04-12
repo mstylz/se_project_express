d46768254f5555352e2f76eca4ad23899d685433
Mesut Yildiz <80417121+mstylz@users.noreply.github.com>
2026-04-12 16:18:11 -0400

    Change error status from 500 to 404 for not found

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const userRouter = require("./users");
 router.use('/items', clothingItem);
 router.use('/users', userRouter);
 router.use((req, res) => {
-  res.status(500).send({ message: "Router Not Found" });
+  res.status(404).send({
+    message: "Requested resource not found",
+  });
 });
 
 module.exports = router;
