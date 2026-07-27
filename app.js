c0e1f1cb64775232397edd535b82c981e3fbd41a
mstylz <mstylz1985@gmail.com>
2026-07-27 11:43:06 -0400

    Use environment variable for JWT secret

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const routes = require("./routes");
+
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -15,17 +17,17 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
 
 // Main routes
 app.use("/", routes);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const JWT_SECRET = "super-strong-secret";
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = {
   JWT_SECRET,
